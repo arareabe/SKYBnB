1c650a05e66424f72bee2270cdc3bb375404fe68
Abe Kim <abe1436@MSI.localdomain>
2022-10-02 12:40:22 -0700

    Final endpoint: get all Spots completed

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -279,6 +279,91 @@ router.post('/:spotId/reviews', requireAuth, async (req, res) => {
   res.json(newReview);
 });
 
+// Get all Spots
+router.get('/', async (req, res) => {
+  let { page, size, minLat, maxLat, minLng, maxLng, minPrice, maxPrice } = req.query;
+
+  page = parseInt(page);
+  size = parseInt(size);
+
+  if (!page) {
+    page = 1
+  };
+
+  if (!size || size > 20) {
+    size = 20
+  };
+
+  if (page < 0 || size < 0 || minPrice < 0 || maxPrice < 0) {
+    return res
+      .status(400)
+      .json({
+        message: 'Validation Error',
+        statusCode: res.statusCode,
+        errors: [{
+          page: 'Page must be greater than or equal to 0',
+          size: 'Size must be greater than or equal to 0',
+          maxLat: 'Maximum latitude is invalid',
+          minLag: 'Minimum latitude is invalid',
+          minLng: 'Maximum longitude is invalid',
+          maxLng: 'Minimum longitude is invalid',
+          minPrice: 'Maximum price must be greater than or equal to 0',
+          maxPrice: 'Minimum price must be greater than or equal to 0'
+        }]
+      })
+  };
+
+
+  let pagination = {};
+
+  if (page >= 0 && size >= 0) {
+    pagination.limit = size;
+    pagination.offset = size * (page - 1)
+  };
+
+  const allSpots = await Spot.findAll({
+    ...pagination
+  });
+
+  const spotsRes = [];
+
+  for (let i = 0; i < allSpots.length; i++) {
+    let currSpot = allSpots[i].toJSON();
+
+    const reviewSum = await Review.sum('stars', {
+      where: { spotId: currSpot.id }
+    })
+
+    const reviewCount = await Review.count({
+      where: { spotId: currSpot.id }
+    });
+
+    if (!reviewSum) {
+      currSpot.avgRating = 'Not yet rated'
+    } else {
+      currSpot.avgRating = (reviewSum / reviewCount).toFixed(1)
+    };
+
+    const prevImage = await SpotImage.findOne({
+      where: {
+        preview: true,
+        spotId: currSpot.id
+      }
+    });
+
+    if (!prevImage) {
+      currSpot.previewImage = 'None available'
+    } else {
+      currSpot.previewImage = prevImage.url
+    };
+
+    spotsRes.push(currSpot);
+  }
+
+  return res.json({ Spots: spotsRes, page, size });
+});
+
+
 // Add an Image to a Spot based on the Spot's ID
 router.post('/:spotId/images', requireAuth, async (req, res) => {
   const { url, preview } = req.body;
