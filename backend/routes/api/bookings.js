db6a1731f3db9849cb04d59d9b05209752bc90d8
Abe Kim <abe1436@MSI.localdomain>
2022-10-02 01:29:17 -0700

    Edit Booking and Delete Booking endpoints added

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -43,3 +43,103 @@ router.get('/current', requireAuth, async (req, res) => {
 
   return res.json({ Bookings: allBookings });
 });
+
+// Edit a Booking
+router.put('/:bookingId', requireAuth, async (req, res) => {
+  const { startDate, endDate } = req.body;
+  const findBooking = await Booking.findByPk(req.params.bookingId);
+
+  if (!startDate || !endDate || endDate <= startDate) {
+    return res
+      .status(400)
+      .json({
+        message: 'Validation Error',
+        statusCode: res.statusCode,
+        errors: [{
+          endDate: 'End date cannot come before Start Date'
+        }]
+      });
+  };
+
+  if (!findBooking) {
+    return res
+      .status(404)
+      .json({
+        message: "Booking couldn't be found",
+        statusCode: res.statusCode
+      });
+  };
+
+  if (endDate < findBooking.endDate) {
+    return res
+      .status(403)
+      .json({
+        message: "Past bookings can't be modified",
+        statusCode: res.statusCode
+      });
+  };
+
+  if (
+    findBooking.startDate >= startDate && findBooking.endDate <= endDate ||
+
+    findBooking.startDate <= startDate && findBooking.endDate >= endDate) {
+
+    return res
+      .status(403)
+      .json({
+        message: 'Sorry, this spot is already booked for the specified dates',
+        statusCode: res.statusCode,
+        errors: [{
+          startDate: 'Start date conflicts with an already existing booking',
+          endDate: 'End date conflicts with an already existing booking'
+        }]
+      });
+  };
+
+  findBooking.startDate = startDate;
+  findBooking.endDate = endDate;
+  findBooking.save();
+
+  return res.json(findBooking);
+});
+
+// Delete a Booking
+router.delete('/:bookingId', requireAuth, async (req, res) => {
+
+  const findBooking = await Booking.findByPk(req.params.bookingId);
+
+  if (!findBooking) {
+    return res
+      .status(404)
+      .json({
+        message: "Booking couldn't be found",
+        statusCode: res.statusCode
+      });
+  };
+
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+  let currDate = `${year}-${month}-${day}`;
+
+  if (findBooking.startDate <= currDate) {
+    return res
+      .status(403)
+      .json({
+        message: "Bookings that have been started can't be deleted",
+        statusCode: res.statusCode
+      });
+  };
+
+  await findBooking.destroy();
+
+  return res
+    .status(200)
+    .json({
+      message: 'Successfully deleted',
+      statusCode: res.statusCode
+    })
+});
+
+module.exports = router;
