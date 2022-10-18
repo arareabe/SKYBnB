03ff71e3ccf06f3bdd7b0aebab318e1d80ac7d79
Abe Kim <abe1436@MSI.localdomain>
2022-10-17 23:41:42 -0700

    added login-form reducer and actions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import * as sessionActions from './store/session';
 
 import configureStore from './store';
 
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
 const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
