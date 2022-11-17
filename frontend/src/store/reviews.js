a270ac6261d3f113a66685b9e68092aa8b007c6b
Abe Kim <abekim143@gmail.com>
2022-11-17 11:05:03 -0800

    added reviews store

---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,15 @@
+import { csrfFetch } from "./csrf";
+
+// TYPES
+const READ_REVIEWS = 'reviews/readReviews';
+const READ_USER_REVIEWS = 'reviews/readUserReviews';
+const CREATE_REVIEW = 'reviews/createReview';
+const REMOVE_REVIEW = 'reviews/removeReview';
+
+// ACTION CREATORS
+
+const createReview = (newReview) =>
+
+// MY THUNKS
+
+// REDUCE ME!
