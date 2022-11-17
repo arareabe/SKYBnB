5bdc9cea897464044be2487c2d7933d66111d40f
Abe Kim <abekim143@gmail.com>
2022-11-17 11:19:28 -0800

    added createReview thunk

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -8,8 +8,41 @@ const REMOVE_REVIEW = 'reviews/removeReview';
 
 // ACTION CREATORS
 
-const createReview = (newReview) =>
+const createReview = (newReview) => {
+  return {
+    type: CREATE_REVIEW,
+    payload: newReview
+  }
+}
 
 // MY THUNKS
 
+export const createAReview = (spotId, newReview) => async dispatch => {
+  const res = await csrfFetch(`/api/spots/${spotId}/reviews`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newReview)
+  });
+
+  if (res.ok) {
+    const createdReview = await res.json();
+    dispatch(createReview(createdReview));
+    return createdReview;
+  }
+}
+
 // REDUCE ME!
+
+const initialState = {
+  spot: {},
+  user: {}
+}
+
+const reviewsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_REVIEW:
+      const createdReviewState = { ...state, spot: { ...state.spot }, user: { ...state.user } };
+      createdReviewState.spot[action.payload.id] = action.payload;
+      return createdReviewState;
+  }
+}
