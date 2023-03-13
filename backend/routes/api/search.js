17ea2388b84135c9eb114a712e281c6d89a86d48
arareabe <abekim143@gmail.com>
2023-03-13 13:11:19 -0700

    added search backend functionality

---
+++ backend/routes/api/search.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const { requireAuth } = require('../../utils/auth');
+const { Spot } = require('../../db/models');
+
+const router = express.Router();
+const { Op } = require('sequelize');
+
+// Get all Spots from search results
+router.get('/search/:searchWord', async (req, res) => {
+  const spots = await Spot.findAll({
+    where: {
+      [Op.or]: [
+        { address: req.params.searchWord},
+        { city: req.params.searchWord},
+        { state: req.params.searchWord},
+        { name: req.params.searchWord}
+      ]
+    }
+  })
+
+  const spotsRes = [];
+
+  for (let i = 0; i < spots.length; i++) {
+    let currSpot = spots[i].toJSON();
+
+    const reviewCount = await Review.count({ where: { spotId: currSpot.id } });
+    const starSum = await Review.sum('stars', { where: { spotId: currSpot.id } });
+
+    if (!starSum) {
+      currSpot.avgRating = 'Not yet rated'
+    } else {
+      currSpot.avgRating = (starSum / reviewCount).toFixed(1);
+    }
+
+    const previewImage = await SpotImage.findOne({
+      where: {
+        [Op.and]: [
+          { spotId: currSpot.id },
+          { preview: true }
+        ]
+      }
+    });
+
+    if (!previewImage) {
+      currSpot.previewImage = 'No images available!'
+    } else {
+      currSpot.previewImage = previewImage.url
+    }
+
+    spotsRes.push(currSpot);
+  }
+
+  return res.json({ Spots: spotsRes });
+})
