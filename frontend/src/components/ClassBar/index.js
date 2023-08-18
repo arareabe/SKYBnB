fd6a7b926db4068eb7bc0339c826d0a83c23895e
arareabe <abekim143@gmail.com>
2023-08-17 23:20:32 -0700

    added ex classbar icons

--- frontend/src/components/ClassBar/index.js
+++ frontend/src/components/ClassBar/index.js
@@ -1,6 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import home100 from '../../pics/home100.png'
+import beach64 from '../../pics/beach100.png'
+import cabin64 from '../../pics/cabin100.png'
+import castle100 from '../../pics/castle100.png'
+import farm100 from '../../pics/farm100.png'
+import lake100 from '../../pics/lake100.png'
+import mansion64 from '../../pics/mansion100.png'
+import room100 from '../../pics/room100.png'
 import { NavLink } from "react-router-dom"
 import './ClassBar.css'
 
@@ -9,17 +16,37 @@ const ClassBar = () => {
 
   return (
     <div className='classBarWrapper'>
-      <NavLink to='/'>
-        <img src={home100} />
-        <div>Class Category Ex.</div>
+      <NavLink className='classBarNav' to='/'>
+        <img className='classBarImg' src={room100} />
+        <div>Rooms</div>
       </NavLink>
-      <NavLink to='/'>
-        <img src={home100} />
-        <div>Class Category Ex.</div>
+      <NavLink className='classBarNav' to='/'>
+        <img className='classBarImg' src={beach64} />
+        <div>Beachfront</div>
       </NavLink>
-      <NavLink to='/'>
-        <img src={home100} />
-        <div>Class Category Ex.</div>
+      <NavLink className='classBarNav' to='/'>
+        <img className='classBarImg' src={home100} />
+        <div>Homes</div>
+      </NavLink>
+      <NavLink className='classBarNav' to='/'>
+        <img className='classBarImg' src={cabin64} />
+        <div>Cabins</div>
+      </NavLink>
+      <NavLink className='classBarNav' to='/'>
+        <img className='classBarImg' src={lake100} />
+        <div>Lakefront</div>
+      </NavLink>
+      <NavLink className='classBarNav' to='/'>
+        <img className='classBarImg' src={mansion64} />
+        <div>Mansions</div>
+      </NavLink>
+      <NavLink className='classBarNav' to='/'>
+        <img className='classBarImg' src={farm100} />
+        <div>Farms</div>
+      </NavLink>
+      <NavLink className='classBarNav' to='/'>
+        <img className='classBarImg' src={castle100} />
+        <div>Castles</div>
       </NavLink>
     </div>
   )
