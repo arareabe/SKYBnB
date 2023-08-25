85973c40027a1f85a6a50d217ca0c15eafaffd59
arareabe <abekim143@gmail.com>
2023-08-24 23:38:30 -0700

    added backend route for claspot

--- backend/routes/api/classes.js
+++ backend/routes/api/classes.js
@@ -13,6 +13,46 @@ router.get('/', async (req, res) => {
 });
 
 // Get All Questions by Class ID
+router.get('/:classId', async (req, res) => {
+  const spots = await Spot.findAll({
+    where: {
+      classId: req.params.classId
+    }
+  });
 
+  const spotsRes = [];
+
+  for (let i = 0; i < spots.length; i++) {
+    let currSpot = spots[i].toJSON();
+
+    const reviewCount = await Review.count({ where: { spotId: currSpot.id } });
+    const starSum = await Review.sum('stars', { where: { spotId: currSpot.id } });
+
+    if (!starSum) {
+      currSpot.avgRating = 'New'
+    } else {
+      currSpot.avgRating = (starSum / reviewCount).toFixed(1);
+    }
+
+    const previewImage = await SpotImage.findOne({
+      where: {
+        [Op.and]: [
+          { spotId: currSpot.id },
+          { preview: true }
+        ]
+      }
+    });
+
+    if (!previewImage) {
+      currSpot.previewImage = 'No images available!'
+    } else {
+      currSpot.previewImage = previewImage.url
+    }
+
+    spotsRes.push(currSpot);
+  }
+
+  return res.json({ Spots: spotsRes });
+});
 
 module.exports = router;
